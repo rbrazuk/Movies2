9f4394288845d866883ee6a0dd723b2ab67b025c
Ross Brazuk <rbrazuk@tonicdesign.com>
2016-03-22 09:43:24 -0400

    commit before adding development branch

--- app/src/main/java/com/example/rbrazuk/movies2/MainActivity.java
+++ app/src/main/java/com/example/rbrazuk/movies2/MainActivity.java
@@ -2,15 +2,25 @@ package com.example.rbrazuk.movies2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.firebase.client.Firebase;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.bt_add_movie) Button btAdd;
+    @Bind(R.id.bt_my_movies) Button btMyMovies;
+    @Bind(R.id.bt_watch_list) Button btWatchList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
 
         Firebase.setAndroidContext(this);
 
